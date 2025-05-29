3a52bfdaee1c750b4a8e1d9f01ef7484ac44bd98
cascallen <cascallensteve@gmail.com>
2025-05-29 16:07:05 +0300

    Update crispy-bootstrap4 package name and version

--- build.sh
+++ build.sh
@@ -2,8 +2,13 @@
 # exit on error
 set -o errexit
 
-# Install dependencies
+# Install Python dependencies
+pip install --upgrade pip
+pip install setuptools wheel
 pip install -r requirements.txt
+
+# Create static directory if it doesn't exist
+mkdir -p staticfiles
 
 # Collect static files
 python manage.py collectstatic --no-input
